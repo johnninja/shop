2b509a3ed48ab3939c2373ab9c37905236354db2
johnninja <1533255996@qq.com>
2017-01-10 01:01:09 +0800

    almost done

---
+++ assets/javascripts/list.js
@@ -0,0 +1,75 @@
+require(['./common'], function(){
+	require(['zepto','lib', 'IScroll' ], function($, lib, IScroll){
+		var myScroll;
+		var loading = $('.loader');
+		var testTemp = '<div class="product-content">' +
+				'<div class="product-header">'+
+					'<h2>生·瓷器<small>手工艺术</small></h2>'+
+				'</div>'+
+				'<div class="summary-info">'+
+					'<p>1976年1月，我到北京探亲，当时，周总理去世不久，人民自发的通过各种形式悼念总理，我亦自觉参与其中。今年是周总理逝世41周年，谨以小诗表达深深地怀念之情。1976年1月，我到北京探亲，当时，周总理去世不久，人民自发的通过各种形式悼念总理，我亦自觉参与其中。今年是周总理逝世41周年，谨以小诗表达深深地怀念之情。</p>'+
+				'</div>'+
+				'<div class="product-banner"><img src="./assets/images/banner.jpg" alt=""></div>'+
+				'<div class="product-price">'+
+					'<div class="d-group">'+
+						'<div class="d-group-wrapper">'+
+							'<div class="d-price">'+
+								'<sub>¥</sub>123'+
+							'</div>'+
+							'<div class="d-like">'+
+								'<i class="iconfont icon-xin1"></i>'+
+								'2000喜欢'+
+							'</div>'+
+						'</div>'+
+					'</div>'+
+					'<div class="buy-btn">'+
+						'<a href="#" class="button red">去购买</a>'+
+					'</div>'+
+				'</div>'+
+			'</div>';
+
+		function initScroll(){
+			myScroll = new IScroll('#list-wrapper',{
+				probeType: 3,
+				preventDefault: false
+			});
+
+			myScroll.scrollToElement('body');
+
+			myScroll.on('scroll', function(){
+				if (this.y < this.maxScrollY + 10) {
+					loading.html('<i class="loader-icon"></i><span>释放完成加载！</span>');
+				}else if(this.y < this.maxScrollY + 30){
+					loading.html('<i class="loader-icon"></i><span>上拉加载更多</span>');
+				}
+			});
+			myScroll.on('scrollEnd', function(){
+				if (this.y <= this.maxScrollY) {
+					pullUpAction();
+				}
+			});
+
+			myScroll.on('refresh', function(){
+				loading.html('<i class="loader-icon"></i><span>上拉加载更多</span>');
+			});
+
+			document.addEventListener('touchmove', function(e){e.preventDefault();}, false);
+			return myScroll;
+		}
+		function pullUpAction(){
+			loading.html('<i class="loader-icon loading"></i><span>加载中...</span>');
+			setTimeout(function(){
+				var el, p, i;
+				el = $('#list-wrapper .products');
+
+				for (i = 0; i < 4; i++) {
+					el.append($(testTemp));
+				}
+				loading.html('<i class="loader-icon"></i><span>上拉加载更多</span>');
+				myScroll.refresh();
+			}, 2000);
+		}
+		initScroll();
+		setTimeout(myScroll.refresh.bind(myScroll),400);
+	});
+});
--- assets/javascripts/tab.js
+++ assets/javascripts/tab.js
@@ -1,9 +1,44 @@
 require(['./common'], function(){
-	require(['zepto'], function($){
+	require(['zepto','./modules/compute'], function($, Compute){
 		$('.tabs .tab').click(function(){
 			var index = $(this).index();
 			$(this).parent().find('.tab').removeClass('active').eq(index).addClass('active');
 			$('.tab-content').hide().eq(index).show();
+		});
+		var colors = $('.p-color').find('.button');
+		var heights = $('.p-height').find('.button');
+
+		colors.click(function(e){
+			colors.removeClass('red');
+			$(this).addClass('red');
+		});
+		heights.click(function(e){
+			heights.removeClass('red');
+			$(this).addClass('red');
+		});
+
+		var newComBox = new Compute({
+			minus: $('.compute-box .minus'),
+			plus: $('.compute-box .plus'),
+			shower: $('.compute-box .shower')
+		});
+		newComBox.init();
+		$('.detail-footer-buttons .button').click(function(e){
+			var modal = $('.modal');
+			modal.show();
+
+			setTimeout(function(){
+				modal.find('.select-good-spec').addClass('active');
+			}, 200);
+		});
+		$('.select-good-spec').find('.close-icon').click(function(e){
+			var modal = $('.modal');
+			
+			modal.find('.select-good-spec').removeClass('active');
+			setTimeout(function(){
+				modal.hide();
+			}, 200);
+
 		});
 	});
 });
